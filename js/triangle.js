11fc88f84e57658b905800d461083e167970814b
Zach Reinhardt <zachreinhardtguy@gmail.com>
2013-05-22 17:47:53 -0400

    Triangle class

---
+++ js/triangle.js
@@ -0,0 +1,24 @@
+//THREE.js doesn't have triangle implemented yet so I have to hand-roll my own
+function Triangle(p1, p2, p3){
+	this.p1 = p1;
+	this.p2 = p2;
+	this.p3 = p3;
+
+	this.geometry = new THREE.Geometry();
+	this.geometry.vertices.push(p1.position);
+	this.geometry.vertices.push(p2.position);
+	this.geometry.vertices.push(p3.position);
+
+	this.geometry.dynamic = true;
+
+	this.geometry.faces.push(new THREE.Face3(0, 2, 1));
+
+	this.material = new THREE.MeshBasicMaterial({color: 0xff0000});
+
+	this.triMesh = new THREE.Mesh(this.geometry, this.material);
+
+	this.timeStep = function(){
+		this.triMesh.geometry.verticesNeedUpdate = true;
+		this.triMesh.geometry.normalsNeedUpdate = true;
+	}
+}
